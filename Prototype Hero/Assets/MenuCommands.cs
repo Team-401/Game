56aeff3452d1d31b94fcf3d00ff06036511177dd
Joel Connell <joel.connell@yahoo.com>
2021-09-13 15:09:19 -0700

    adding title screne, more sounds, and added mainmenu buttons with functionality, and imported soudns

---
+++ Prototype Hero/Assets/MenuCommands.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCommands : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ToMainMenu()
+    {
+
+        SceneManager.LoadScene(0);
+    }
+
+}
