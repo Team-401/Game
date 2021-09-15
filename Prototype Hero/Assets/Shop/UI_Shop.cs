4361f31d127d2715d7ad2fa37b4a3a96cee6c2b2
miriam silva <miriam-silva@live.com>
2021-09-15 11:37:37 -0700

    Added Shop Menu Clickabilty &  functions to subtract money

--- Prototype Hero/Assets/Shop/UI_Shop.cs
+++ Prototype Hero/Assets/Shop/UI_Shop.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Shop : MonoBehaviour
 {
     [SerializeField] private GameObject shopBox;
 
+    public Button BuySwordButton;
+    public Button BuyCharmButton;
+    public Button BuyPotionButton;
+
+    public int moneyAmount;
+
     private void Start()
     {
+        moneyAmount = 500;
         Close();
+    }
+    public void Update()
+    {
+        
     }
     public void Open()
     {
@@ -17,5 +29,48 @@ public class UI_Shop : MonoBehaviour
     public void Close()
     {
         shopBox.SetActive(false);
+    }
+
+    public void BuyPotion()
+    {
+        if(moneyAmount >= 25)
+        {
+            moneyAmount = moneyAmount - 25;
+            Debug.Log("Potion purchased, money: " + moneyAmount);
+        }
+        else
+        {
+         Text text =  BuyPotionButton.GetComponentInChildren<Text>();
+            text.text = "Not enough coins!";
+        }
+
+    }
+    public void BuyCharm()
+    {
+        if (moneyAmount >= 200)
+        {
+            moneyAmount = moneyAmount - 200;
+            Debug.Log("Charm purchased, money: " + moneyAmount);
+        }
+        else
+        {
+            Text text = BuyCharmButton.GetComponentInChildren<Text>();
+            text.text = "Not enough coins!";
+        }
+
+    }
+    public void BuySword()
+    {
+        if (moneyAmount >= 250)
+        {
+            moneyAmount = moneyAmount - 250;
+            Debug.Log("Sword purchased, money: " + moneyAmount);
+        }
+        else
+        {
+            Text text = BuySwordButton.GetComponentInChildren<Text>();
+            text.text = "Not enough coins!";
+        }
+
     }
 }
