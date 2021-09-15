a64757cc0f05fac79d001fb6523bb5531f76512c
Ben Arno <bdarno92@gmail.com>
2021-09-15 11:47:28 -0700

    Boss AI partial implementation, hitbox on point, bandits moved out of resources folder

--- Prototype Hero/Assets/Combat/Necromancer - Pixel Art/Demo/Necromancer.cs
+++ Prototype Hero/Assets/Combat/Necromancer - Pixel Art/Demo/Necromancer.cs
@@ -3,15 +3,28 @@ using System.Collections;
 
 public class Necromancer : MonoBehaviour {
 
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
+    [SerializeField] float      m_speed = 3f;
+    //[SerializeField] float      m_jumpForce = 7.5f;
     [SerializeField] bool       m_noBlood = false;
 
     private Animator            m_animator;
     private Rigidbody2D         m_body2d;
     private Sensor_Necromancer  m_groundSensor;
+    private BoxCollider2D       m_boxCollider;
     private bool                m_grounded = false;
     private float               m_delayToIdle = 0.0f;
+
+    public int MaxHealth = 100;
+    public float detectionRange;
+    public float smallestApproachDistance;
+    public float attackStartDistance;
+    public LayerMask playerLayer;
+    public Transform attackPointBasic;
+    public float attackHitRange;
+
+    private bool m_isDead;
+    private int _currentHealth;
+    private Transform player;
 
     // Use this for initialization
     void Start ()
@@ -19,6 +32,10 @@ public class Necromancer : MonoBehaviour {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
         m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Necromancer>();
+        m_boxCollider = GetComponent<BoxCollider2D>();
+
+        _currentHealth = MaxHealth;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -38,20 +55,30 @@ public class Necromancer : MonoBehaviour {
             m_animator.SetBool("Grounded", m_grounded);
         }
 
-        // -- Handle input and movement --
-        float inputX = Input.GetAxis("Horizontal");
+        // ========== Direction Decisions ==========
+
+        //Calculating direction variables
+        Vector2 newLocation = Vector2.MoveTowards(this.transform.position, player.position, m_speed * Time.deltaTime);
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        float playerDirection = this.transform.position.x - player.position.x;
+
+        Debug.Log(distanceFromPlayer);
 
         // Swap direction of sprite depending on walk direction
-        if (inputX > 0)
-            GetComponent<SpriteRenderer>().flipX = false;
-        else if (inputX < 0)
-            GetComponent<SpriteRenderer>().flipX = true;
+        if (playerDirection > 0)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else if (playerDirection < 0)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
 
         // Move
-        m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+        //m_body2d.velocity = new Vector2(playerDirection * m_speed, m_body2d.velocity.y);
 
         //Set AirSpeed in animator
-        m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
+        //m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
 
         // -- Handle Animations --
         //Death
@@ -74,21 +101,24 @@ public class Necromancer : MonoBehaviour {
             m_animator.SetTrigger("Spellcast");
 
         //Jump
-        else if (Input.GetKeyDown("space") && m_grounded)
-        {
-            m_animator.SetTrigger("Jump");
-            m_grounded = false;
-            m_animator.SetBool("Grounded", m_grounded);
-            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
-            m_groundSensor.Disable(0.2f);
-        }
+        //else if (Input.GetKeyDown("space") && m_grounded)
+        //{
+        //    m_animator.SetTrigger("Jump");
+        //    m_grounded = false;
+        //    m_animator.SetBool("Grounded", m_grounded);
+        //    m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+        //    m_groundSensor.Disable(0.2f);
+        //}
 
-        //Run
-        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
+        //4: Run
+        //Trigger if player in range
+        else if (distanceFromPlayer < detectionRange && distanceFromPlayer > smallestApproachDistance)
         {
-            // Reset timer
-            m_delayToIdle = 0.05f;
             m_animator.SetInteger("AnimState", 1);
+
+            //This is not animation, this is the actual movement of the player
+            transform.position = newLocation;
+
         }
 
         //Idle
@@ -99,5 +129,44 @@ public class Necromancer : MonoBehaviour {
                 if(m_delayToIdle < 0)
                     m_animator.SetInteger("AnimState", 0);
         }
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+        // Put in a text or something like that to show damage over the bandits head
+        _currentHealth -= damage;
+        Debug.Log("Enemy health at " + _currentHealth);
+
+        m_animator.SetTrigger("Hurt");
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        m_isDead = true;
+        Debug.Log("The enemy is dead!");
+
+        Destroy(m_body2d);
+        Destroy(m_boxCollider);
+
+        Invoke("cleanupDeath", 2.0f);
+    }
+
+    void cleanupDeath()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPointBasic.position, attackHitRange);
     }
 }
