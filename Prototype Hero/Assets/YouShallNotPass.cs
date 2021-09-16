7efce58ac35c62e94cfb8dd4924ab0c57ccf69c6
Joshua Haddock <joshhaddock88@gmail.com>
2021-09-16 12:01:15 -0700

    Create Boss Wall script

---
+++ Prototype Hero/Assets/YouShallNotPass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouShallNotPass : MonoBehaviour
+{
+    public GameObject magicBarrier;
+    public UiCharm charmUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(charmUI.HasCharm())
+        {
+            magicBarrier.SetActive(false);
+        }
+    }
+}
